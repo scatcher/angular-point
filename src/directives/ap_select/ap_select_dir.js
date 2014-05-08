635b835cddb77bf8f93d136d460fe7707b45a8eb
Scott Hatcher <github@scotthatcher.com>
2014-05-08 12:01:11 -0700

    Updated ap_select parameter name

--- src/directives/ap_select/ap_select_dir.js
+++ src/directives/ap_select/ap_select_dir.js
@@ -7,14 +7,18 @@ angular.module('angularPoint')
             replace: true,
             templateUrl: 'src/directives/ap_select/ap_select_tmpl.html',
             scope: {
-                bindedField: '=',   //The field on the model to bind to
+                target: '=',   //The field on the model to bind to
+                bindedField: '=',   //Deprecated....why did I use binded instead of bound?
                 multi: '=',         //Single select if not set or set to false
                 arr: '=',           //Array of lookup options
                 lookupValue: '=',   //Field name to map the lookupValue to (default: 'title')
                 ngDisabled: '='     //Pass through to disable control using ng-disabled on element if set
             },
             link: function (scope) {
-
+                if(scope.bindedField && !scope.target) {
+                    //Todo remove all references to "bindedField" and change to target
+                    scope.target = scope.bindedField;
+                }
                 scope.state = {
                     multiSelectIDs: [],
                     singleSelectID: ''
@@ -45,36 +49,36 @@ angular.module('angularPoint')
 
                 scope.updateMultiModel = function () {
                     /** Ensure field being binded against is array */
-                    if (!_.isArray(scope.bindedField)) {
-                        scope.bindedField = [];
+                    if (!_.isArray(scope.target)) {
+                        scope.target = [];
                     }
                     /** Clear out existing contents */
-                    scope.bindedField.length = 0;
+                    scope.target.length = 0;
                     /** Push formatted lookup object back */
                     _.each(scope.state.multiSelectIDs, function (stringId) {
-                        scope.bindedField.push(buildLookupObject(stringId));
+                        scope.target.push(buildLookupObject(stringId));
                     });
                 };
 
                 scope.updateSingleModel = function () {
                     /** Create an object with expected lookupId/lookupValue properties */
-                    scope.bindedField = buildLookupObject(scope.state.singleSelectID);
+                    scope.target = buildLookupObject(scope.state.singleSelectID);
                 };
 
                 /** Process initially and whenever the underlying value is changed */
-                scope.$watch('bindedField', function() {
+                scope.$watch('target', function() {
                     if (scope.multi) {
                         /** Multi Select Mode
                          *  Set the string version of id's to allow multi-select control to work properly */
-                        _.each(scope.bindedField, function (selectedLookup) {
+                        _.each(scope.target, function (selectedLookup) {
                             /** Push id as a string to match what Select2 is expecting */
                             scope.state.multiSelectIDs.push(selectedLookup.lookupId.toString());
                         });
                     } else {
                         /** Single Select Mode */
-                        if (_.isObject(scope.bindedField) && scope.bindedField.lookupId) {
+                        if (_.isObject(scope.target) && scope.target.lookupId) {
                             /** Set the selected id as string */
-                            scope.state.singleSelectID = scope.bindedField.lookupId;
+                            scope.state.singleSelectID = scope.target.lookupId;
                         }
                     }
                 });
