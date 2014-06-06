9cdeaeee723209947c7f7cf3766d441f9c44ff4f
Scott Hatcher <scott@scotthatcher.com>
2014-06-06 14:41:03 -0700

    Added a check for users running IE8 in compatibility mode.

--- dist/ie-safe.js
+++ dist/ie-safe.js
@@ -3903,6 +3903,12 @@ var toObject = function (o) {
  * Polyfill to prevent debugging code from throwing errors in older IE
  */
 (function () {
+    /** Ensure that IE users in IE8 aren't in compatibility mode, notify if they are */
+    if(document.documentMode && document.documentMode < 8) {
+        alert("You need to have compatibility mode disabled to use this application.  " +
+            "Please go to the tools menu and deselect \"Compatibility View\".");
+    }
+
     if (!window.console) {
         window.console = {};
     }
