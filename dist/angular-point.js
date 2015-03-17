236bb0928acfeac76009e24178d887c1a1e4674b
Scott Hatcher <scott@scotthatcher.com>
2015-03-17 13:08:28 -0700

    Expose encodeValue method.

--- dist/angular-point.js
+++ dist/angular-point.js
@@ -477,6 +477,110 @@ angular.module('angularPoint')
 'use strict';
 
 /**
+ * @ngdoc service
+ * @name angularPoint.apUserModel
+ * @description
+ * Simple service that allows us to request and cache both the current user and their group memberships.
+ *
+ * @requires apDataService
+ *
+ */
+angular.module('angularPoint')
+    .service('apUserModel', ['$q', '_', 'apDataService', function ($q, _, apDataService) {
+
+        var model = {
+                checkIfMember: checkIfMember,
+                getGroupCollection: getGroupCollection,
+                getUserProfile: getUserProfile
+            },
+            /** Local references to cached promises */
+            _getGroupCollection,
+            _getUserProfile;
+
+        return model;
+
+
+        /**
+         * @ngdoc function
+         * @name angularPoint.apUserModel:getUserProfile
+         * @methodOf angularPoint.apUserModel
+         * @description
+         * Returns the user profile for the current user and caches results.
+         * Pull user profile info and parse into a profile object
+         * http://spservices.codeplex.com/wikipage?title=GetUserProfileByName
+         * @param {boolean} [force=false] Ignore any cached value.
+         * @returns {object} Promise which resolves with the requested user profile.
+         */
+        function getUserProfile(force) {
+            if (!_getUserProfile || force) {
+                /** Create a new deferred object if not already defined */
+                _getUserProfile = apDataService.getUserProfileByName();
+            }
+            return _getUserProfile;
+        }
+
+
+        /**
+         * @ngdoc function
+         * @name angularPoint.apUserModel:getGroupCollection
+         * @methodOf angularPoint.apUserModel
+         * @description
+         * Returns the group names for the current user and caches results.
+         * @param {boolean} [force=false] Ignore any cached value.
+         * @returns {string[]} Promise which resolves with the array of groups the user belongs to.
+         */
+        function getGroupCollection(force) {
+            if (!_getGroupCollection || force) {
+                /** Create a new deferred object if not already defined */
+                var deferred = $q.defer();
+                getUserProfile(force).then(function (userProfile) {
+                    apDataService.getGroupCollectionFromUser(userProfile.userLoginName)
+                        .then(function (groupCollection) {
+                            deferred.resolve(groupCollection);
+                        });
+                });
+                _getGroupCollection = deferred.promise;
+            }
+            return _getGroupCollection;
+        }
+
+
+        /**
+         * @ngdoc function
+         * @name angularPoint.apUserModel:checkIfMember
+         * @methodOf angularPoint.apUserModel
+         * @description
+         * Checks to see if current user is a member of the specified group.
+         * @param {string} groupName Name of the group.
+         * @param {boolean} [force=false] Ignore any cached value.
+         * @returns {boolean} Is the user a member of the group?
+         */
+        function checkIfMember(groupName, force) {
+            //Allow function to be called before group collection is ready
+            var deferred = $q.defer();
+            var self = this;
+
+            //Initially ensure groups are ready, any future calls will receive the return
+            model.getGroupCollection(force).then(function (groupCollection) {
+                //Data is ready
+                //Map the group names to cache results for future calls, rebuild if data has changed
+                if (!self.groupMap || self.groupMap.length !== groupCollection.length) {
+                    self.groupMap = [];
+                    _.each(groupCollection, function (group) {
+                        self.groupMap.push(group.Name);
+                    });
+                }
+                deferred.resolve(_.isObject(groupCollection[self.groupMap.indexOf(groupName)]));
+            });
+
+            return deferred.promise;
+        }
+
+    }]);
+
+'use strict';
+
+/**
  * @ngdoc function
  * @name angularPoint.apCamlFactory
  * @description
@@ -3375,110 +3479,6 @@ angular.module('angularPoint')
 
 /**
  * @ngdoc service
- * @name angularPoint.apUserModel
- * @description
- * Simple service that allows us to request and cache both the current user and their group memberships.
- *
- * @requires apDataService
- *
- */
-angular.module('angularPoint')
-    .service('apUserModel', ['$q', '_', 'apDataService', function ($q, _, apDataService) {
-
-        var model = {
-                checkIfMember: checkIfMember,
-                getGroupCollection: getGroupCollection,
-                getUserProfile: getUserProfile
-            },
-            /** Local references to cached promises */
-            _getGroupCollection,
-            _getUserProfile;
-
-        return model;
-
-
-        /**
-         * @ngdoc function
-         * @name angularPoint.apUserModel:getUserProfile
-         * @methodOf angularPoint.apUserModel
-         * @description
-         * Returns the user profile for the current user and caches results.
-         * Pull user profile info and parse into a profile object
-         * http://spservices.codeplex.com/wikipage?title=GetUserProfileByName
-         * @param {boolean} [force=false] Ignore any cached value.
-         * @returns {object} Promise which resolves with the requested user profile.
-         */
-        function getUserProfile(force) {
-            if (!_getUserProfile || force) {
-                /** Create a new deferred object if not already defined */
-                _getUserProfile = apDataService.getUserProfileByName();
-            }
-            return _getUserProfile;
-        }
-
-
-        /**
-         * @ngdoc function
-         * @name angularPoint.apUserModel:getGroupCollection
-         * @methodOf angularPoint.apUserModel
-         * @description
-         * Returns the group names for the current user and caches results.
-         * @param {boolean} [force=false] Ignore any cached value.
-         * @returns {string[]} Promise which resolves with the array of groups the user belongs to.
-         */
-        function getGroupCollection(force) {
-            if (!_getGroupCollection || force) {
-                /** Create a new deferred object if not already defined */
-                var deferred = $q.defer();
-                getUserProfile(force).then(function (userProfile) {
-                    apDataService.getGroupCollectionFromUser(userProfile.userLoginName)
-                        .then(function (groupCollection) {
-                            deferred.resolve(groupCollection);
-                        });
-                });
-                _getGroupCollection = deferred.promise;
-            }
-            return _getGroupCollection;
-        }
-
-
-        /**
-         * @ngdoc function
-         * @name angularPoint.apUserModel:checkIfMember
-         * @methodOf angularPoint.apUserModel
-         * @description
-         * Checks to see if current user is a member of the specified group.
-         * @param {string} groupName Name of the group.
-         * @param {boolean} [force=false] Ignore any cached value.
-         * @returns {boolean} Is the user a member of the group?
-         */
-        function checkIfMember(groupName, force) {
-            //Allow function to be called before group collection is ready
-            var deferred = $q.defer();
-            var self = this;
-
-            //Initially ensure groups are ready, any future calls will receive the return
-            model.getGroupCollection(force).then(function (groupCollection) {
-                //Data is ready
-                //Map the group names to cache results for future calls, rebuild if data has changed
-                if (!self.groupMap || self.groupMap.length !== groupCollection.length) {
-                    self.groupMap = [];
-                    _.each(groupCollection, function (group) {
-                        self.groupMap.push(group.Name);
-                    });
-                }
-                deferred.resolve(_.isObject(groupCollection[self.groupMap.indexOf(groupName)]));
-            });
-
-            return deferred.promise;
-        }
-
-    }]);
-
-'use strict';
-
-/**
- * @ngdoc service
  * @name angularPoint.apCacheService
  * @description
  * Stores a reference for all list items based on list GUID and list item id.  Allows us to then register promises that
@@ -5572,6 +5572,7 @@ angular.module('angularPoint')
         return {
             choiceMultiToString: choiceMultiToString,
             createValuePair: createValuePair,
+            encodeValue: encodeValue,
             generateValuePairs: generateValuePairs,
             stringifySharePointDate: stringifySharePointDate,
             stringifySharePointMultiSelect: stringifySharePointMultiSelect
@@ -5648,6 +5649,14 @@ angular.module('angularPoint')
             return [fieldDefinition.staticName, encodedValue];
         }
 
+        /**
+         * @ngdoc function
+         * @name angularPoint.apEncodeService:encodeValue
+         * @methodOf angularPoint.apEncodeService
+         * @param {string} fieldType One of the valid field types.
+         * @param {*} value Value to be encoded.
+         * @returns {string} Encoded value ready to be sent to the server.
+         */
         function encodeValue(fieldType, value) {
             var str = '';
             /** Only process if note empty, undefined, or null.  Allow false. */
