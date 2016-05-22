7c6cef0ba15edd617b284e31f385ae7e76b3f8d3
Scott Hatcher <scott@scotthatcher.com>
2016-05-22 08:33:51 -0700

    Updated build info.

--- dist/index.js
+++ dist/index.js
@@ -7,26 +7,11 @@ function __export(m) {
  * @description
  * Starting point to import all public core APIs.
  */
-// import 'rxjs/add/observable/map';
-// import 'rxjs/add/observable/combineLatest';
 require('rxjs/add/observable/forkJoin');
-// import 'rxjs/add/observable/from';
-// import 'rxjs/add/observable/fromPromise';
-// import 'rxjs/add/operator/catch';
-// import 'rxjs/add/operator/combineLatest';
-// import 'rxjs/add/operator/debounce';
-// import 'rxjs/add/operator/defaultIfEmpty';
-// import 'rxjs/add/operator/distinctUntilChanged';
 require('rxjs/add/operator/do');
-// import 'rxjs/add/operator/do';
 require('rxjs/add/operator/map');
 require('rxjs/add/operator/mergeMap');
-// import 'rxjs/add/operator/pluck';
 require('rxjs/add/operator/retry');
-// import 'rxjs/add/operator/share';
-// import 'rxjs/add/operator/skipWhile';
-// import 'rxjs/add/operator/toPromise';
-// import 'rxjs/add/operator/withLatestFrom';
 __export(require('./constants'));
 __export(require('./services'));
 __export(require('./providers'));
